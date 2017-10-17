1081ef35b6b080bde1cbc29d42ba53eb9bb533dd
Barry <barryboayes17@gmail.com>
2017-10-17 11:28:34 -0700

    initial commit

---
+++ server/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Controller
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
