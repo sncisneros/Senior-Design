d52cff180408760929243cdaedb66908f474590b
sasha cisneros <sfierce07@gmail.com>
2017-10-18 17:41:59 -0700

    data model classes - proposal incomplete

---
+++ data model/College.java
@@ -0,0 +1,18 @@
+class College{
+	
+	String name;
+	String dean;
+
+	College(String name, String dean){
+	this.name = name;
+	this.dean = dean;
+	}
+
+	String getName(){
+		return name;
+	}
+
+	String getDean(){
+		return dean;
+	}
+}
---
+++ data model/Department.java
@@ -0,0 +1,24 @@
+class Department{
+	
+	String name;
+	String depChair;
+	String college;
+
+	Department(String name. String depChair, String college){
+		this.name= name;
+		this.depCair = depChair;
+		this.college = college;
+	}
+
+	String getName(){
+		return name;
+	}
+
+	String getDepChair(){
+		return depChair;
+	}
+
+	String getCollege(){
+		return college;
+	}
+}
