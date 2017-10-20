b174e708b3dc48b083d093cbb50e1f24711f4458
sasha cisneros <sfierce07@gmail.com>
2017-10-20 12:14:08 -0700

    user, college, department update

---
+++ data model/Proposal.java
@@ -0,0 +1,14 @@
+class Proposal{
+	//incomplete
+
+	String piName;
+	String projectTitle;
+	String department;
+	String college;
+	String sponsor;
+	Date startDate;
+	Date endDate;
+	double budget;
+	int copies;
+	String summary;
+}
